f3bb7d9eef37cb330892a2828e513aca2b20a945
martinluther <martinlutherly@gmail.com>
2026-08-25 22:14:43 +0700

    fix

--- backend/server.py
+++ backend/server.py
@@ -8,6 +8,7 @@ from datetime import datetime, timezone, timedelta
 from typing import List, Optional
 
 import requests
+import certifi
 from fastapi import FastAPI, APIRouter, HTTPException, Header, Cookie, Response, UploadFile, File, Query, Request
 from fastapi.responses import Response as FastAPIResponse
 from dotenv import load_dotenv
@@ -19,7 +20,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / ".env")
 
 mongo_url = os.environ["MONGO_URL"]
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(mongo_url, tls=True, tlsCAFile=certifi.where())
 db = client[os.environ["DB_NAME"]]
 
 APP_NAME = "undangan-digital"
